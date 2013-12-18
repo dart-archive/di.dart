f7295af6ec49d92549c1374387a35b1a5d20067d
Pavel Jbanov <pavelgj@gmail.com>
2013-12-18 10:23:21 -0800

    chore(benchmark): split benchmarks into separate files

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -2,11 +2,17 @@
 set -e
 
 # run tests in dart
-dart benchmark/injector_benchmark.dart
+dart benchmark/module_benchmark.dart
+dart benchmark/dynamic_injector_benchmark.dart
+dart benchmark/static_injector_benchmark.dart
 
 # run dart2js on tests
 mkdir -p out
-dart2js -c benchmark/injector_benchmark.dart   -o out/injector_benchmark.dart.js
+dart2js -c benchmark/module_benchmark.dart   -o out/module_benchmark.dart.js
+dart2js -c benchmark/static_injector_benchmark.dart   -o out/static_injector_benchmark.dart.js
+dart2js -c benchmark/dynamic_injector_benchmark.dart   -o out/dynamic_injector_benchmark.dart.js
 
 # run tests in node
-node out/injector_benchmark.dart.js
+node out/module_benchmark.dart.js
+node out/static_injector_benchmark.dart.js
+node out/dynamic_injector_benchmark.dart.js
