f869bc447fbe805ff7b6af0dc375274f52698994
Pavel Jbanov <pavelgj@gmail.com>
2013-12-18 10:49:46 -0800

    chore(benchmark): fixed runner

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -8,11 +8,11 @@ dart benchmark/static_injector_benchmark.dart
 
 # run dart2js on tests
 mkdir -p out
-dart2js -c benchmark/module_benchmark.dart   -o out/module_benchmark.dart.js
-dart2js -c benchmark/static_injector_benchmark.dart   -o out/static_injector_benchmark.dart.js
-dart2js -c benchmark/dynamic_injector_benchmark.dart   -o out/dynamic_injector_benchmark.dart.js
+dart2js --minify benchmark/module_benchmark.dart   -o out/module_benchmark.dart.js
+dart2js --minify benchmark/static_injector_benchmark.dart   -o out/static_injector_benchmark.dart.js
+dart2js --minify benchmark/dynamic_injector_benchmark.dart   -o out/dynamic_injector_benchmark.dart.js
 
 # run tests in node
 node out/module_benchmark.dart.js
-node out/static_injector_benchmark.dart.js
 node out/dynamic_injector_benchmark.dart.js
+node out/static_injector_benchmark.dart.js
