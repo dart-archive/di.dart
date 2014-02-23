66fdb88214257cb7d5527d9415cb8d8aa7ef197b
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-02-23 22:30:01 +1100

    Fixed wrong benchmark run for js version

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -14,7 +14,7 @@ $BENCHMARK $ENDPOINT_URL $(dart benchmark/static_injector_benchmark.dart)
 
 # run dart2js on tests
 mkdir -p out
-dart2js --minify benchmark/ideal_injector_benchmark.dart -o out/no_injector_benchmark.dart.js
+dart2js --minify benchmark/no_injector_benchmark.dart -o out/no_injector_benchmark.dart.js
 dart2js --minify benchmark/ideal_injector_benchmark.dart -o out/ideal_injector_benchmark.dart.js
 dart2js --minify benchmark/module_benchmark.dart -o out/module_benchmark.dart.js
 dart2js --minify benchmark/static_injector_benchmark.dart -o out/static_injector_benchmark.dart.js
