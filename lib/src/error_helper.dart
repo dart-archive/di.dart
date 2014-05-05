f673267dd2eb3a3058ec8657e4f034057e377c47
Chris Calabro <ccalabro@google.com>
2014-05-05 09:53:31 -0400

    fix(Key): fixed bugs caused by hashCode collisions, and docs cleanup
    Fixes #94

--- lib/src/error_helper.dart
+++ lib/src/error_helper.dart
@@ -3,21 +3,19 @@ library di.error_helper;
 import 'package:di/di.dart';
 import 'package:di/src/base_injector.dart';
 
-String error(ResolutionContext resolving, message, [Key appendDependency]) {
+/**
+ * Returns an error message for the given dependency chain.
+ * If [addDependency] is given, its [String] representation gets appended
+ * to the error message.
+ *
+ * Example:
+ * If [resolving]'s ancestors have keys k1, k2, k3 and [message] is 'foo',
+ * then this looks like 'foo (resolving k3 -> k2 -> k1)'.
+ */
+String error(ResolutionContext resolving, String message, [Key appendDependency]) {
   if (appendDependency != null) {
     resolving = new ResolutionContext(resolving.depth + 1, appendDependency, resolving);
   }
-
-  String graph = resolvedTypes(resolving).reversed.join(' -> ');
-
-  return '$message (resolving $graph)';
-}
-
-List<Key> resolvedTypes(ResolutionContext resolving) {
-  List resolved = [];
-  while (resolving.depth != 0) {
-    resolved.add(resolving.key);
-    resolving = resolving.parent;
-  }
-  return resolved;
+  String path = resolving.ancestorKeys().reversed.join(' -> ');
+  return '$message (resolving $path)';
 }
--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -2,7 +2,7 @@ part of di;
 
 abstract class Injector {
   /**
-   * Name of the injector or null of none is given.
+   * Name of the injector or null if none was given.
    */
   String get name;
 
@@ -17,49 +17,67 @@ abstract class Injector {
   Injector get root;
 
   /**
-   * List of all types which the injector can return
+   * [Type]s of the [Provider]s explicitly bound to the injector or an ancestor.
+   * If the root injector sets [allowImplicitInjection] to false, then this
+   * is simply all the types that the injector can return.
    */
   Set<Type> get types;
 
   /**
-   * Indicates whether injector allows implicit injection -- resolving types
-   * that were not explicitly bound in the module(s).
+   * Whether the injector allows injecting a type to which no [Provider] is
+   * bound.  Note that this setting only matters for the root injector.
    */
   bool get allowImplicitInjection;
 
   /**
-   * Get an instance for given token ([Type]).
+   * Returns the instance associated with the given key (i.e. [type] and
+   * [annotation]) according to the following rules.
    *
-   * If the injector already has an instance for this token, it returns this
-   * instance. Otherwise, injector resolves all its dependencies, instantiates
-   * new instance and returns this instance.
+   * Let I be the nearest ancestor injector (possibly this one) that both
    *
-   * If there is no binding for given token, injector asks parent injector.
+   * - binds some [Provider] P to [key] and
+   * - P's visibility declares that I is visible to this injector.
    *
-   * If there is no parent injector, an implicit binding is used. That is,
-   * the token ([Type]) is instantiated.
+   * If there is no such I, then
+   *
+   * - if [allowImplicitInjection] is true for the root injector, let I be the
+   *   root injector and P be a default [Provider] for [type].
+   * - if [allowImplicitInjection] is false for the root injector, throw
+   *   [NoProviderError].
+   *
+   * Once I and P are found, if I already created an instance for the key,
+   * it is returned.  Otherwise, P is used to create an instance, using I
+   * as an [ObjectFactory] to resolve the necessary dependencies.
    */
   dynamic get(Type type, [Type annotation]);
 
   /**
-   * Get an instance for given key ([Key]).
-   *
-   * If the injector already has an instance for this key, it returns this
-   * instance. Otherwise, injector resolves all its dependencies, instantiates
-   * new instance and returns this instance.
-   *
-   * If there is no binding for given key, injector asks parent injector.
+   * See [get].
    */
   dynamic getByKey(Key key);
 
   /**
-   * Create a child injector.
+   * Creates a child injector.
    *
-   * Child injector can override any bindings by adding additional modules.
+   * [modules] overrides bindings of the parent.
    *
-   * It also accepts a list of tokens that a new instance should be forced.
-   * That means, even if some parent injector already has an instance for this
-   * token, there will be a new instance created in the child injector.
+   * [forceNewInstances] is a list, each element of which is a [Key] or a
+   * [Type] (for convenience when no annotation is needed).  For each element K,
+   * the child injector will have a new binding to the same [Provider] P that
+   * the parent injector would have provided, that is, P is the [Provider] of
+   * the nearest ancestor of the parent injector that binds K.  Note that this
+   * differs from how [get] finds P in that visibility is not taken into
+   * account.
+   *
+   * Thus, if a descendant D of the child requests an instance for K, the child
+   * will mask any binding for K made by a proper ancestor injector, provided
+   * that P's visbility reveals the child's binding to D.
+   * For example, if the child has no proper descendant and P's visibility
+   * deems that the child is visible to the child itself, then the first
+   * request for the child to get an instance for K will trigger the creation of
+   * a new instance.
+   *
+   * [name] is used for error reporting.
    */
   Injector createChild(List<Module> modules,
                        {List forceNewInstances, String name});
