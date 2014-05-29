33df7c30e629af4e2a02ce8acc1ce121e63fc04d
Marko Vuksanovic <markovuksanovic@gmail.com>
2014-05-29 23:04:52 +1000

    feat(TypeLiteral): Added TypeLiteral which can be used to bind types which use generics (parameterized types).

--- lib/di.dart
+++ lib/di.dart
@@ -2,8 +2,10 @@ library di;
 
 import 'src/provider.dart';
 import 'key.dart';
+import 'type_literal.dart';
 
 export 'key.dart' show Key;
+export 'type_literal.dart' show TypeLiteral;
 
 part 'src/injector.dart';
 part 'src/module.dart';
