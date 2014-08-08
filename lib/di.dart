19c72e903fb767e4ded338da75ec1214762cad1f
Anting Shen <anting@google.com>
2014-08-08 11:21:44 -0700

    chore(annotations): remove annotation export

--- lib/di.dart
+++ lib/di.dart
@@ -5,4 +5,3 @@ export 'src/injector.dart' show Injector, ModuleInjector;
 export 'src/module.dart' show Module, Binding, DEFAULT_VALUE;
 export 'src/reflector.dart' show TypeReflector;
 export 'src/errors.dart' hide BaseError, PRIMITIVE_TYPES;
-export 'annotations.dart';
