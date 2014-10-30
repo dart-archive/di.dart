090bebebe9dcfab9f8ea8228cc54ac2c352af65b
Chris Bracken <chris@bracken.jp>
2014-10-29 17:20:06 -0700

    fix(benchmarks): Get dart2js'ed benchmarks running on node

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -1,5 +1,18 @@
-#!/bin/sh 
+#!/bin/bash
 set -e
+
+# Prepend text to a file in-place.
+function di::prepend_text {
+  local file=$1
+  local text=$2
+
+  # NOTE: sed -i doesn't work on osx/bsd
+  tmpfile=$(mktemp -t di_benchmark.XXXXXX)
+  echo "$text" > $tmpfile
+  cat "$file" >> $tmpfile
+  cp -f "$tmpfile" "$file"
+  rm -f "$tmpfile"
+}
 
 BENCHMARKS="module_benchmark.dart
 dynamic_injector_benchmark.dart
@@ -11,21 +24,30 @@ mkdir -p benchmark/generated_files
 dart scripts/class_gen.dart
 
 # run tests in dart
-for b in $BENCHMARKS
-do
-    dart benchmark/$b
+echo "Dart VM Benchmarks:"
+echo "-------------------"
+for b in $BENCHMARKS; do
+  echo "Running: $b"
+  dart benchmark/$b
 done
 
 # run dart2js on tests
+echo
+echo "Compiling with dart2js:"
+echo "-----------------------"
 mkdir -p out
-echo "running dart2js"
-for b in $BENCHMARKS
-do
-    dart2js --minify benchmark/$b   -o out/$b.js > /dev/null
+for b in $BENCHMARKS; do
+  echo "$b"
+  dart2js --minify benchmark/$b -o out/$b.js > /dev/null
+  # HACK node.js doesn't understand self
+  di::prepend_text "out/$b.js" "var self=this"
 done
 
 # run tests in node
-for b in $BENCHMARKS
-do
-    node out/$b.js
+echo
+echo "JS Benchmarks:"
+echo "--------------"
+for b in $BENCHMARKS; do
+  echo "Running: $b"
+  node out/$b.js
 done
